81652e32d9e578e91a75b865a579e1f86db8ebfb
Nick Wilson <nick@njwilson.net>
2011-09-13 22:46:41 -0700

    Initial project layout

---
+++ README.rst
@@ -0,0 +1,6 @@
+Library for the Nirvana Task Manager
+====================================
+
+This Python library provides access to Nirvana, the web-based task
+management software from NirvanaHQ (nirvanahq.com). Nirvana is based on
+David Allen's popular Getting Things Done (GTD) methodology.
---
+++ setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import nirvana
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+with open('README.rst') as readme:
+    long_description = readme.read()
+
+setup(
+    name='nirvana',
+    version=nirvana.__version__,
+    description=('Library for interacting with the Nirvana task manager '
+                 '(nirvanahq.com)'),
+    long_description=long_description,
+    author='Nick Wilson',
+    author_email='nick@njwilson.net',
+    url='http://github.com/njwilson/nirvana-python',
+    license='MIT',
+    packages=['nirvana'],
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
