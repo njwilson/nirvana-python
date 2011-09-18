54c9471a0f6898c462df6f61f20cd7249262cd18
Nick Wilson <nick@njwilson.net>
2011-09-17 20:58:11 -0700

    Enable Sphinx TODO plugin
    This allows us to use '.. todo::' directives (similar to notes) that only
    show up when the documentation is built with the TODOs enabled. TODOs are
    enabled for now. They'll probably be disabled once the documentation is
    more complete.

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,10 @@ from nirvana import __version__
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'sphinx.ext.todo']
+
+# sphinx.ext.todo configuration
+todo_include_todos=True
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
